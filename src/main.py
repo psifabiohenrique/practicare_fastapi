4401106798c34f5882d97107524b157062055b84
Fábio Henrique <psifabiohenrique@outlook.com>
2025-12-29 01:56:49 -0300

    Added cors configurations and removed database from repository

--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import auth, users
 
 app = FastAPI(title="Practicare FastAPI")
 
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:5173",
+]
+
 app.include_router(auth.router)
 app.include_router(users.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
