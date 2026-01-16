52acd05097540318df393e72b935f5d21d038c19
Fábio Henrique <psifabiohenrique@outlook.com>
2026-01-16 00:07:56 -0300

    Created controller route to reupload audio file to treatment record that already exists.

--- src/routers/treatment_record_controller.py
+++ src/routers/treatment_record_controller.py
@@ -118,6 +118,51 @@ async def upload_audio(
     return record
 
 
+@router.post(
+    "/treatments/{treatment_record_uuid}/automated-record-reload",
+    response_model=TreatmentRecordRead,
+)
+async def reload_audio(
+    treatment_record_uuid: UUID,
+    db: SessionDB,
+    current_user: CurrentUser,
+    audio_file: UploadFile = File(...),
+):
+
+    user_uuid = current_user.uuid
+    if audio_file.content_type not in {
+        "audio/webm",
+        "video/webm",
+        "audio/ogg",
+    }:
+        raise HTTPException(
+            HTTPStatus.BAD_REQUEST, detail="Invalid audio content type"
+        )
+
+    record = await TreatmentRecordService.get_treatment_record(
+        db=db, treatment_record_uuid=treatment_record_uuid, user_uuid=user_uuid
+    )
+
+    job = await AutomatedRecordService.create_job(
+        db=db,
+        treatment_uuid=record.treatment_uuid,
+        user_uuid=user_uuid,
+        treatment_record_uuid=record.uuid,
+    )
+
+    audio_bytes = await audio_file.read()
+
+    transcribe_audio.delay(job_uuid=job.uuid, audio=audio_bytes)
+    record = await TreatmentRecordService.update_treatment_record(
+        db,
+        record.uuid,
+        user_uuid,
+        TreatmentRecordUpdate(content="Reprocessando o áudio, aguarde..."),
+    )
+
+    return record
+
+
 @router.patch("/{treatment_record_uuid}", response_model=TreatmentRecordRead)
 async def update_treatment_record(
     treatment_record_uuid: UUID,
