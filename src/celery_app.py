0f34c3f6ae66aa05ecc8fad8a037f6ec1d2ed891
Fábio Henrique <psifabiohenrique@outlook.com>
2026-01-18 15:33:08 -0300

    Created docker compose to prod.

--- src/celery_app.py
+++ src/celery_app.py
@@ -5,7 +5,6 @@ from src.settings import settings
 celery_app = Celery(
     "practicare",
     broker=settings.REDIS_URL,
-    backend=settings.REDIS_URL,
 )
 
 # celery_app.autodiscover_tasks(["src.tasks"])
